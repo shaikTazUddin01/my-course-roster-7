afccb8dfc23eeffabf653af63165fee52f3321e2
sheikhtazuddin <tazahmedsoft@gmail.com>
2023-09-15 01:34:55 +0600

    added 9 course with full  details

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
+// import { useState } from "react"
+import Courses from "./Component/Courses/Courses"
+
 import Header from "./Component/Header/Header";
+
+
 function App() {
-  <div className="my-10 container mx-auto ">
-  <Header></Header>
-</div>
 
 
-
+  return (
+    <div className="my-10 container mx-auto ">
+      <Header></Header>
+      <div className="flex gap-5">
+        <div className="w-4/5">
+          <Courses ></Courses>
+        </div>
+        <div>
+        </div>
+      </div>
+    </div>
+  )
 }
 export default App
